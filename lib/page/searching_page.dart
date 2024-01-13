b235ce789353c13ea2dd90a1812e0fe34398cc97
lodny <drinkjuice@naver.com>
2024-01-13 14:44:27 +0900

    5. - use AppBar: bottom - use PreferredSize - use TabBar, TabController - use TickerProviderStateMixin

--- lib/page/searching_page.dart
+++ lib/page/searching_page.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_clone_instagram/imagepath.dart';
 import 'package:get/get.dart';
 
-class SearchingPage extends StatelessWidget {
+class SearchingPage extends StatefulWidget {
   const SearchingPage({super.key});
+
+  @override
+  State<SearchingPage> createState() => _SearchingPageState();
+}
+
+class _SearchingPageState extends State<SearchingPage> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 5, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +29,7 @@ class SearchingPage extends StatelessWidget {
           ),
           onTap: Get.back,
         ),
+        titleSpacing: 0,
         title: Container(
           margin: EdgeInsets.only(right: 20),
           // padding: EdgeInsets.symmetric(horizontal: 10),
@@ -24,23 +38,54 @@ class SearchingPage extends StatelessWidget {
             color: Color(0xffefefef),
           ),
           child: TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              border: InputBorder.none,
-              hintText: '검색',
-              isDense: true,
-            )
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(
+                    vertical: 5, horizontal: 10),
+                border: InputBorder.none,
+                hintText: '검색',
+                isDense: true,
+              )
           ),
         ),
-        titleSpacing: 0,
+        bottom: _tabMenu(),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
+      body: _body(),
+    );
+  }
 
-          ],
+  PreferredSize _tabMenu() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(AppBar().preferredSize.height),
+      child: Container(
+        height: AppBar().preferredSize.height,
+        width: Size.infinite.width,
+        child: TabBar(
+          controller: tabController,
+          indicatorColor: Colors.black,
+          indicatorSize: TabBarIndicatorSize.tab,
+          tabs: _tabMenuList(),
         ),
       ),
+    );
+  }
+
+  List<Widget> _tabMenuList() {
+    List<String> menuText = ['인기', '계정', '오디오', '태그', '장소'];
+
+    return menuText.map((text) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          child: Text(
+            '인기',
+            style: TextStyle(fontSize: 15, color: Colors.black),
+          ),
+        ),
+      )
+      .toList();
+  }
+
+  Widget _body() {
+    return Container(
+      color: Colors.red,
     );
   }
 }
